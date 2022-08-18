d28246dbbaeb5d972922a1d564ba3f6c2f61f7a7
Inivncible2022 <invictus19994@gmail.com>
2022-08-18 20:31:49 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,7 +30,10 @@ const questions = [
 function Result({ correct }) {
   return (
     <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"  alt="conf"/>
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"
+        alt="conf"
+      />
       <h2>
         Вы отгадали {correct} ответа из {questions.length}
       </h2>
