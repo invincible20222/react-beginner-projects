477f8763ba3cd0027dd069ed3faa7c31fad00d9b
Inivncible2022 <invictus19994@gmail.com>
2022-08-18 22:51:22 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./index.scss";
 import { Success } from "./components/Success";
 import { Users } from "./components/Users";
 
-// Тут список пользователей: https://reqres.in/api/users
-
 function App() {
+  const [users, setUsers] = React.useState([]);
+  const [invites, setInvites] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [success, setSuccess] = React.useState(false);
+  const [searchValue, setSearchValue] = React.useState("");
+
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Ошибка при получении пользователей");
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites((prev) => prev.filter((_id) => _id !== id));
+    } else {
+      setInvites((prev) => [...prev, id]);
+    }
+  };
+
+  const onClickSendInvites = () => {
+    setSuccess(true);
+  };
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {success ? (
+        <Success count={invites.length} />
+      ) : (
+        <Users
+          onChangeSearchValue={onChangeSearchValue}
+          searchValue={searchValue}
+          items={users}
+          isLoading={isLoading}
+          invites={invites}
+          onClickInvite={onClickInvite}
+          onClickSendInvites={onClickSendInvites}
+        />
+      )}
     </div>
   );
 }
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import successImg from "../assets/success.svg";
 
 export const Success = ({ count }) => {
   return (
-    <div class="success-block">
-      <img src="/assets/success.svg" alt="Success" />
+    <div className="success-block">
+      <img src={successImg} alt="Success" />
       <h3>Успешно!</h3>
       <p>Всем {count} пользователям отправлено приглашение.</p>
-      <button className="send-invite-btn">Назад</button>
+      <button
+        onClick={() => window.location.reload()}
+        className="send-invite-btn"
+      >
+        Назад
+      </button>
     </div>
   );
 };
